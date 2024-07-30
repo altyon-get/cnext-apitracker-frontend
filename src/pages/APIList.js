1e98314a220c0cb98fd33267efa8c44693e6d4be
altyon_get <subham.ojha@careers360.in>
2024-07-30 23:29:07 +0530

    add search functionality

--- src/pages/APIList.js
+++ src/pages/APIList.js
@@ -17,9 +17,8 @@ const APIList = ({ openModal }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(12);
   const [searchTerm, setSearchTerm] = useState("");
-  const [sortBy, setSortBy] = useState("updated_at");
-  const [sortOrder, setSortOrder] = useState("asc");
   const [totalApis, setTotalApis] = useState(0);
+  
 
   useEffect(() => {
     fetchApis();
@@ -32,6 +31,7 @@ const APIList = ({ openModal }) => {
         params: {
           page: currentPage,
           page_size: itemsPerPage,
+          search_term: searchTerm,
         },
       });
       setApis(response?.data?.data);
@@ -79,18 +79,6 @@ const APIList = ({ openModal }) => {
       )
     );
   };
-
-  const filteredAndSortedApis = useMemo(() => {
-    return apis
-      .filter((api) =>
-        api.endpoint.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-      .sort((a, b) => {
-        if (a[sortBy] < b[sortBy]) return sortOrder === "desc" ? -1 : 1;
-        if (a[sortBy] > b[sortBy]) return sortOrder === "desc" ? 1 : -1;
-        return 0;
-      });
-  }, [apis, searchTerm, sortBy, sortOrder]);
 
 
   const formatDate = (dateString) => {
@@ -168,7 +156,7 @@ const APIList = ({ openModal }) => {
     <div className="flex flex-col h-screen">
       <h2 className="text-2xl font-bold mb-6 text-gray-800">API List</h2>
 
-      <div className="mb-4 flex flex-wrap items-center justify-between">
+      <div className="mb-4 flex flex-wrap items-center gap-4">
         <div className="w-full md:w-1/3 mb-4 md:mb-0">
           <input
             type="text"
@@ -178,168 +166,159 @@ const APIList = ({ openModal }) => {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
-        <div className="w-full md:w-2/3 flex flex-wrap justify-end items-center">
-          <select
-            value={sortBy}
-            onChange={(e) => setSortBy(e.target.value)}
-            className="mr-2 px-3 py-2 border border-gray-300 rounded-md"
-          >
-            <option value="updated_at">Sort by Last Updated</option>
-            <option value="endpoint">Sort by Endpoint</option>
-            <option value="status">Sort by Status</option>
-          </select>
-          <button
-            onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}
-            className="px-3 py-2 bg-gray-200 rounded-md flex items-center justify-center"
-          >
-            {sortOrder === "asc" ? "▲" : "▼"}
-          </button>
-        </div>
+        <button
+          onClick={fetchApis}
+          disabled={isLoading}
+          className={`w-fit py-2 px-2 font-bold text-white rounded-lg ${
+            isLoading ? "bg-gray-400" : "bg-blue-500 hover:bg-blue-700"
+          } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50`}
+        >
+          Search
+        </button>
       </div>
 
       <div className="flex flex-col justify-between h-full">
-      
-      {isLoading ? (
-        <Loader />
-      ) : filteredAndSortedApis.length === 0 ? (
-        <div className="text-center py-10">
-          <p className="text-xl text-gray-600">
-            No APIs found. Please add some APIs or adjust your filters.
-          </p>
-        </div>
-      ) : (
-        <>
-          <div className="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
-            <table className="min-w-full bg-white">
-              <thead>
-                <tr className="w-full bg-gray-100 border-b-2 border-gray-300">
-                  <th className="w-1/24 py-2  px-2 text-left text-gray-600 font-normal">
-                    #
-                  </th>
-                  <th className="w-7/12 py-2  text-left text-gray-600 font-normal ">
-                    Endpoint
-                  </th>
-                  <th className="w-1/12 py-2 px-2 text-left text-gray-600 font-normal">
-                    Method
-                  </th>
-                  <th className="w-1/12 py-2 px-2 text-left text-gray-600 font-normal">
-                    Status
-                  </th>
-                  <th className="w-1/12 py-2 px-2 text-left text-gray-600  font-normal">
-                    Code
-                  </th>
-                  <th className="w-1/12 py-2 px-2 text-left text-gray-600  font-normal">
-                    Latency(s)
-                  </th>
-                  <th className="w-1/12 py-2 px-2 text-left text-gray-600  font-normal">
-                    Updated
-                  </th>
-                  <th className="w-1/12 py-2 px-2 text-left text-gray-600 font-normal">
-                    Actions
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                {filteredAndSortedApis.map((api, index) => (
-                  <tr
-                    className="border-b border-gray-200 hover:bg-gray-50 cursor-pointer"
-                    onClick={() => handleRowClick(api._id)}
-                    key={api._id}
-                  >
-                    <td className="py-2 px-2 text-sm">
-                      {(currentPage - 1) * itemsPerPage + index + 1}
-                    </td>
-                    <td
-                      className="py-2  max-w-[200px] overflow-x-auto thin-scrollbar text-sm"
+        {isLoading ? (
+          <Loader />
+        ) : apis.length === 0 ? (
+          <div className="text-center py-10">
+            <p className="text-xl text-gray-600">
+              No APIs found. Please add some APIs or adjust your filters.
+            </p>
+          </div>
+        ) : (
+          <>
+            <div className="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
+              <table className="min-w-full bg-white">
+                <thead>
+                  <tr className="w-full bg-gray-100 border-b-2 border-gray-300">
+                    <th className="w-1/24 py-2  px-2 text-left text-gray-600 font-normal">
+                      #
+                    </th>
+                    <th className="w-7/12 py-2  text-left text-gray-600 font-normal ">
+                      Endpoint
+                    </th>
+                    <th className="w-1/12 py-2 px-2 text-left text-gray-600 font-normal">
+                      Method
+                    </th>
+                    <th className="w-1/12 py-2 px-2 text-left text-gray-600 font-normal">
+                      Status
+                    </th>
+                    <th className="w-1/12 py-2 px-2 text-left text-gray-600  font-normal">
+                      Code
+                    </th>
+                    <th className="w-1/12 py-2 px-2 text-left text-gray-600  font-normal">
+                      Latency(s)
+                    </th>
+                    <th className="w-1/12 py-2 px-2 text-left text-gray-600  font-normal">
+                      Updated
+                    </th>
+                    <th className="w-1/12 py-2 px-2 text-left text-gray-600 font-normal">
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white">
+                  {apis.map((api, index) => (
+                    <tr
+                      className="border-b border-gray-200 hover:bg-gray-50 cursor-pointer"
+                      onClick={() => handleRowClick(api._id)}
+                      key={api._id}
                     >
-                      {getHighlightedText(api.endpoint, searchTerm)}
-                    </td>
-                    <td
-                      className={`py-2 px-4 flex text-sm ${
-                        api.method === "GET"
-                          ? "text-green-400 font-semibold"
-                          : api.method === "POST"
-                          ? "text-blue-400 font-semibold"
-                          : api.method === "PUT"
-                          ? "text-yellow-600 font-semibold"
-                          : api.method === "DELETE"
-                          ? "text-red-300 font-semibold"
-                          : "text-gray-500"
-                      }`}
-                    >
-                      {api.method}
-                    </td>
-                    <td className="py-2 px-4 ">
-                      <span
-                        className={`px-2 inline-flex text-s leading-5 font-semibold rounded-full ${
-                          api.status
-                            ? "bg-green-100 text-green-800"
-                            : "bg-red-100 text-red-800"
+                      <td className="py-2 px-2 text-sm">
+                        {(currentPage - 1) * itemsPerPage + index + 1}
+                      </td>
+                      <td className="py-2  max-w-[200px] overflow-x-auto thin-scrollbar text-sm">
+                        {getHighlightedText(api.endpoint, searchTerm)}
+                      </td>
+                      <td
+                        className={`py-2 px-4 text-sm ${
+                          api.method === "GET"
+                            ? "text-green-400 font-semibold"
+                            : api.method === "POST"
+                            ? "text-blue-400 font-semibold"
+                            : api.method === "PUT"
+                            ? "text-yellow-600 font-semibold"
+                            : api.method === "DELETE"
+                            ? "text-red-300 font-semibold"
+                            : "text-gray-500"
                         }`}
                       >
-                        {api.status ? <FiCheck /> : <FiX />}
-                      </span>
-                    </td>
-                    <td className="py-2 px-4 text-sm  ">{api.code || "-"}</td>
-                    <td className="py-2 px-4 text-sm ">{api.response_time}</td>
-                    <td className="py-2 px-2 text-sm">
-                      {formatDate(api.updated_at)}
-                    </td>
-                    <td className="py-2 px-2 flex space-x-4 my-2">
-                      <Link
-                        to={`/edit-api/${api._id}`}
-                        className="text-gray-500 hover:text-gray-700"
-                        title="Edit API"
-                        onClick={(e) => e.stopPropagation()}
-                      >
-                        <AiFillEdit size={18} />
-                      </Link>
-                      <button
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          handleDeleteClick(api._id);
-                        }}
-                        className="text-gray-500 hover:text-gray-700"
-                        title="Delete API"
-                      >
-                        <FiTrash2 size={18} />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </>
-      )}
-      <div className="flex justify-center items-center mt-6 gap-12">
-        <button
-          onClick={() => currentPage > 1 && paginate(currentPage - 1)}
-          className={`px-3 py-2 bg-gray-200 rounded-md ${
-            currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          disabled={currentPage === 1}
-        >
-          <FaChevronLeft />
-        </button>
+                        {api.method}
+                      </td>
+                      <td className="py-2 px-4 ">
+                        <span
+                          className={`px-2 inline-flex text-s leading-5 font-semibold rounded-full ${
+                            api.status
+                              ? "bg-green-100 text-green-800"
+                              : "bg-red-100 text-red-800"
+                          }`}
+                        >
+                          {api.status ? <FiCheck /> : <FiX />}
+                        </span>
+                      </td>
+                      <td className="py-2 px-4 text-sm  ">{api.code || "-"}</td>
+                      <td className="py-2 px-4 text-sm ">
+                        {api.response_time}
+                      </td>
+                      <td className="py-2 px-2 text-sm">
+                        {formatDate(api.updated_at)}
+                      </td>
+                      <td className="py-2 px-2 flex space-x-4 my-2">
+                        <Link
+                          to={`/edit-api/${api._id}`}
+                          className="text-gray-500 hover:text-gray-700"
+                          title="Edit API"
+                          onClick={(e) => e.stopPropagation()}
+                        >
+                          <AiFillEdit size={18} />
+                        </Link>
+                        <button
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleDeleteClick(api._id);
+                          }}
+                          className="text-gray-500 hover:text-gray-700"
+                          title="Delete API"
+                        >
+                          <FiTrash2 size={18} />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
+        <div className="flex justify-center items-center mt-6 gap-12">
+          <button
+            onClick={() => currentPage > 1 && paginate(currentPage - 1)}
+            className={`px-3 py-2 bg-gray-200 rounded-md ${
+              currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={currentPage === 1}
+          >
+            <FaChevronLeft />
+          </button>
 
-        <div className="flex space-x-1">{renderPaginationButtons()}</div>
+          <div className="flex space-x-1">{renderPaginationButtons()}</div>
 
-        <button
-          onClick={() =>
-            currentPage < Math.ceil(totalApis / itemsPerPage) &&
-            paginate(currentPage + 1)
-          }
-          className={`px-3 py-2 bg-gray-200 rounded-md ${
-            currentPage === Math.ceil(totalApis / itemsPerPage)
-              ? "opacity-50 cursor-not-allowed"
-              : ""
-          }`}
-          disabled={currentPage === Math.ceil(totalApis / itemsPerPage)}
-        >
-          <FaChevronRight />
-        </button>
-      </div>
+          <button
+            onClick={() =>
+              currentPage < Math.ceil(totalApis / itemsPerPage) &&
+              paginate(currentPage + 1)
+            }
+            className={`px-3 py-2 bg-gray-200 rounded-md ${
+              currentPage === Math.ceil(totalApis / itemsPerPage)
+                ? "opacity-50 cursor-not-allowed"
+                : ""
+            }`}
+            disabled={currentPage === Math.ceil(totalApis / itemsPerPage)}
+          >
+            <FaChevronRight />
+          </button>
+        </div>
       </div>
     </div>
   );
