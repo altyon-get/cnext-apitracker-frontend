104c62e5446ac3e00c3c1b4b935d55d51ca97166
altyon_get <subham.ojha@careers360.in>
2024-08-02 20:05:06 +0530

    corrected path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "cnext_frontend",
-        script: "./index.js",
+        script: "src/index.js",
         instances: 2,
         exec_mode: "cluster",
         env: {
