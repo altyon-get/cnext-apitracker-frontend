2af13df4997349a4ad395478e758c133cfe27c3a
altyon_get <subham.ojha@careers360.in>
2024-08-05 16:34:57 +0530

    add console in config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const config = {
     apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:8000'
 };
-  
+console.log('Environment Variables:', JSON.stringify(process.env, null, 2));
 export default config;
-  
