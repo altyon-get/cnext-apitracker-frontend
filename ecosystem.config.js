c4597a4e997e07deba2534a407a40867b4f18fda
altyon_get <subham.ojha@careers360.in>
2024-08-05 15:37:23 +0530

    updated ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps: [
       {
-        name: "cnext_frontend",
-        script: "src/index.js",
+        name: "cnext-apitracker-frontend",
+        script: "/src/index.js",
         instances: 2,
         exec_mode: "cluster",
         env: {
